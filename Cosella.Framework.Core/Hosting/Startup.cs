1b8b4745ee86e6487bef8ce552e6896de2acb398
Mark Jose <mjose@scottlogic.com>
2017-09-26 12:02:49 +0100

    Add null value handling for outputted JSON

--- Cosella.Framework.Core/Hosting/Startup.cs
+++ Cosella.Framework.Core/Hosting/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Cosella.Framework.Core.Integrations.Swagger;
 using Cosella.Framework.Core.VersionTracking;
 using Microsoft.Owin.Cors;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using Ninject;
 using Ninject.Web.Common.OwinHost;
@@ -29,6 +30,7 @@ namespace Cosella.Framework.Core.Hosting
 
             config.Formatters.Remove(config.Formatters.XmlFormatter);
             config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
             config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
             config.Formatters.Add(config.Formatters.JsonFormatter);
 
